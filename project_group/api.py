425e2947bfd6f241889e678835035150b95c8d05
calvin_nathanael <calvinyuxy@gmail.com>
2022-07-24 12:33:52 +0800

    api - commit

--- project_group/api.py
+++ project_group/api.py
@@ -1,2 +1,6 @@
-print('hello word does this no')
-print("hello, can yall see? -jolin")
+import requests
+
+currency_url = 'https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey=0I9X3CH0ID27ONE2'
+r = requests.get(currency_url)
+data = r.json()
+
