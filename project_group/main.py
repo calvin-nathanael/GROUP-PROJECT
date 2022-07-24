50f8803f6ebf671778921d8aa04729e3605abe15
calvin_nathanael <calvinyuxy@gmail.com>
2022-07-24 20:26:44 +0800

    'main.py uploaded.'

--- project_group/main.py
+++ project_group/main.py
@@ -1,1 +1,10 @@
-print('eh herro')
+from api import mean_forex_closing_price
+from read_file import process_csv_file
+from write_file import report_deficit
+
+def main():
+    avg_cp = mean_forex_closing_price()
+    days, cash, net_profit, overheads = process_csv_file()
+    report_deficit(days, cash, net_profit, overheads, avg_cp)
+
+main()
