b42ae2ff3c7cdddfec0b6fb012aa47e564278d0d
calvin! <108908516+calvin-nathanael@users.noreply.github.com>
2022-08-03 23:21:00 +0800

    added exception handling

--- project_group/overheads.py
+++ project_group/overheads.py
@@ -7,25 +7,37 @@ def overheads_function(forex):
     oh_path = Path.cwd()/"csv_reports"/"Overheads.csv"
     report_path = Path.cwd()/"summary_report.txt"
 
-    # opening the file
-    with oh_path.open(mode="r", encoding = "UTF-8") as file:
-        reader = csv.reader(file)
-        next(reader)
+    # using exception handling
+    try:
+        # opening the file
+        with oh_path.open(mode="r", encoding = "UTF-8") as file:
+            reader = csv.reader(file)
+            next(reader)
 
-        # appending contents of the file to a list, and the overheads in the list
-        list = []
-        overheads_list = []
-        for line in reader:
-            list.append(line)
-            overheads_list.append(float(line[1]))
+            # appending contents of the file to a list, and the overheads in the list
+            list = []
+            overheads_list = []
+            for line in reader:
+                list.append(line)
+                overheads_list.append(float(line[1]))
 
-        # using for loop and if function to match a overhead to the highest overhead
-        for category, overheads in list:
-            if float(overheads) == max(overheads_list):
+            # using for loop and if function to match a overhead to the highest overhead
+            for category, overheads in list:
+                if float(overheads) == max(overheads_list):
 
-                # extracting the overhead and corresponding category and writing it in the summary report, and converting the overhead to SGD using forex
-                with report_path.open(mode="a") as file:
-                    file.write(f"\n[HIGHEST OVERHEADS] {category}: SGD{round((float(overheads)*forex),2)}")
-                    file.close()
+                    # extracting the overhead and corresponding category and writing it in the summary report, and converting the overhead to SGD using forex
+                    with report_path.open(mode="a") as file:
+                        file.write(f"\n[HIGHEST OVERHEADS] {category}: SGD{round((float(overheads)*forex),2)}")
+                        file.close()
 
-        file.close()
+            file.close()
+
+    # if the above code fails, it will run the following  
+    except:
+        with report_path.open(mode="a") as file:
+                    file.write("\n[OVERHEADS File Error] File is missing or not renamed properly. Ensure that the file is named 'Overheads.csv'")
+                    file.close() 
+
+    # after everything, continue.
+    else:
+        pass
